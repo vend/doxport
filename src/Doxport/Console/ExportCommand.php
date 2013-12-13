29600449e1aebc95cdb2b99bcaa9576303ea8e68
Dominic Scheirlinck <dominic@vendhq.com>
2013-12-13 17:29:27 +1300

    Basic stubbing of export command

--- src/Doxport/Console/ExportCommand.php
+++ src/Doxport/Console/ExportCommand.php
@@ -2,7 +2,9 @@
 
 namespace Doxport\Console;
 
+use Doxport\CriteriaBuilder;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -12,13 +14,21 @@ class ExportCommand extends Command
     {
         $this
             ->setName('export')
-            ->addOption(
-                'bootstrap'
-            )
-            ->setDescription('Exports a set of data from the database');
+            ->addArgument('entity', InputArgument::REQUIRED, 'The entity to begin exporting from', null)
+            ->addArgument('id', InputArgument::REQUIRED, 'The entity that the export will start from', null)
+            ->setDescription('Exports a set of data from the database, beginning with a specified type');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        /* @var \Doctrine\ORM\EntityManager $em */
+        $em = $this->getHelper('em')->getEntityManager();
+
+        $criteria = (new CriteriaBuilder($em))
+            ->from($input->getArgument('entity'))
+            ->id($input->getArgument('id'))
+            ->build();
+
+        echo (string)$criteria;
     }
 }
