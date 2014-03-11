d72a9ace3e58977d51b638d93489ad97ede875c4
Dominic Scheirlinck <dominic@vendhq.com>
2014-03-11 17:22:18 +1300

    Test setting options

--- test/Doxport/DoxportTest.php
+++ test/Doxport/DoxportTest.php
@@ -6,10 +6,29 @@ class DoxportTest extends AbstractMockTest
 {
     public function testConstructor()
     {
-        $doxport = new Doxport($this->getMockEntityManager());
+        $manager = $this->getMockEntityManager();
+        $doxport = new Doxport($manager);
 
         // Defaults if not injected
         $this->assertInstanceOf('Doxport\Metadata\Driver', $doxport->getMetadataDriver());
         $this->assertInstanceOf('Doxport\File\Factory', $doxport->getFileFactory());
+
+        // Same instance
+        $this->assertEquals($manager, $doxport->getEntityManager());
+    }
+
+    public function testSetOptions()
+    {
+        $doxport = new Doxport($this->getMockEntityManager());
+        $doxport->setOptions([
+            'root'  => true,
+            'image' => false
+        ]);
+    }
+
+    public function testSetOption()
+    {
+        $doxport = new Doxport($this->getMockEntityManager());
+        $doxport->setOption('root', true);
     }
 }
