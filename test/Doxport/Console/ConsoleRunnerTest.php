4caea0fe50800de3a5d0729ddc81ff0123049b7d
Dominic Scheirlinck <dominic@vendhq.com>
2014-06-16 15:33:21 +1200

    Test console runner with mock application

--- test/Doxport/Console/ConsoleRunnerTest.php
+++ test/Doxport/Console/ConsoleRunnerTest.php
@@ -2,8 +2,9 @@
 
 namespace Doxport\Console;
 
-use Doxport\Test\AbstractTest;
 use Doxport\Exception\Exception;
+use Doxport\Test\AbstractTest;
+use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Helper\HelperSet;
 
 class ConsoleRunnerTest extends AbstractTest
@@ -13,9 +14,46 @@ class ConsoleRunnerTest extends AbstractTest
      */
     public function testNoEntityManager()
     {
+        $runner = new ConsoleRunner();
         $helper = new HelperSet([]);
 
-        $runner = new ConsoleRunner();
         $runner->run($helper);
+    }
+
+    public function testRun()
+    {
+        $runner = new ConsoleRunner();
+        $helper = new HelperSet([$this->getMockEmHelper()]);
+
+        $application = $this->getMockApplication();
+
+        $application->expects($this->once())
+            ->method('run');
+
+        $runner->run($helper, $application);
+    }
+
+    /**
+     * @return Application
+     */
+    protected function getMockApplication()
+    {
+        $mock = $this->getMockBuilder('Symfony\Component\Console\Application')
+            ->getMock();
+
+        return $mock;
+    }
+
+    protected function getMockEmHelper()
+    {
+        $mock = $this->getMockBuilder('Symfony\Component\Console\Helper\HelperInterface')
+            //->setConstructorArgs([['em' => $this->getMockEntityManager()]])
+            ->getMockForAbstractClass();
+
+        $mock->expects($this->any())
+            ->method('getName')
+            ->will($this->returnValue('em'));
+
+        return $mock;
     }
 }
